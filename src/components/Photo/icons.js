180500509c0a33beb577ef26261d3652522cad0d
Michael Wood <mdwood@algomapublichealth.com>
2018-08-31 15:45:59 -0400

    feat: added icon kit

---
+++ src/components/Photo/icons.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { user } from "react-icons-kit/fa/";
+import { withBaseIcon } from "react-icons-kit";
+
+const Icon = withBaseIcon({ size: 196 });
+export const UserIcon = () => <Icon icon={user} />;
